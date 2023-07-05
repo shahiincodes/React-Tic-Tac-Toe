ba6fd0b3df90fd9b176756db3c4279a30d859d8e
Habib <shahin.amla@gmail.com>
2023-07-06 01:13:09 +0530

    Winner function written and the codding is finished

--- tic-tac-toe-game/src/components/Board/Board.js
+++ tic-tac-toe-game/src/components/Board/Board.js
@@ -7,11 +7,15 @@ const Board = () => {
     const [isX,setIsx] = useState(true)
     const handleClick = (i)=>{
         const newSquares =  square.slice()
+
         if(square[i]){
             alert("This box is already filled")
             return
         }
-        if (isX) {
+        if(Winner){
+            return 
+        }
+        if(isX) {
             newSquares[i] = 'X'
         } else {
             newSquares[i] = 'O'
@@ -19,9 +23,29 @@ const Board = () => {
         setSquare(newSquares)
         setIsx(!isX)
     }
-  
+  function winner (square){
+    let lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    for(let i=0;i<lines.length;i++){
+        const [a,b,c] = lines[i]
+        if(square[a]&&square[a]===square[b]&&square[a]===square[c]){
+            return square[a]
+        }
+    }
+    return null
+  }
+  const Winner = winner(square)
   return (
     <div className='board'>
+        <h1>Winner:  {Winner}</h1>
         <div className="lines">
             <Square value={square[0]} onSquareClick = {()=>handleClick(0)} />
             <Square value={square[1]} onSquareClick = {()=>handleClick(1)} />
